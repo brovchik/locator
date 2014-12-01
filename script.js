f0dcdb3cafb98f737d43e94b09bd35595a08e27e
brev <you@example.com>
2014-12-01 18:05:21 +0300

    css and js added

---
+++ script.js
@@ -0,0 +1,9 @@
+function HideOverrlay() {
+	document.getElementById("overl").style.display='none';
+	
+}
+
+function showAsides() {
+	document.getElementById("left-aside").style.display="block";
+	document.getElementById("right-aside").style.display="block";
+}
