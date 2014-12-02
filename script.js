5d24b6b6ea206970a9852cb69a6f34c7a69bf27b
brev <you@example.com>
2014-12-02 18:34:25 +0300

    modified form. Fixed JS

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function HideOverrlay() {
-	document.getElementById("overl").className="hidden";	
+function HideOverlay() {
+	var name = document.forms[0].userName.value;
+		var pass = document.forms[0].pass.value;
+	setTimeout((function(){
+		console.log(name);
+		console.log(pass);
+		if(name == "BREV" && pass =="BREV") {
+			document.getElementById("header").innerHTML = "<div class=\"logined\">Привет, "+name+"!</div>";
+			document.getElementById("overl").className="hidden";
+		}	
+		else {alert("Invalid user "+name+"! Enter again");}
+		
+	}), 1000);
 }
 
 function showAsides() {
@@ -13,7 +24,8 @@ function showAsides() {
 	else document.getElementById("right-aside").style.display = "none";
 }
 
-function fact(n) {
-	if(n == 0 || n == 1) return 1;
-	else return n * fact(n-1);
-}
+// function fact(n) {
+// 	if(n == 0 || n == 1) return 1;
+// 	else return n * fact(n-1);
+// }
+
