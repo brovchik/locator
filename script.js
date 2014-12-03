086d2a647e55ca4a37a8891b74d35c1fa93fbf66
brev <you@example.com>
2014-12-03 18:04:24 +0300

    added black variant for coordinates and time

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-function HideOverlay() {
-	var name = document.forms[0].userName.value;
-		var pass = document.forms[0].pass.value;
-	setTimeout((function(){
-		console.log(name);
-		console.log(pass);
-		if(name == "BREV" && pass =="BREV") {
-			document.getElementById("header").innerHTML += "<div class=\"logined\">Привет, "+name+"!</div>";
-			document.getElementById("overl").className="hidden";
-		}	
-		else {alert("Invalid user "+name+"! Enter again");}
+// function HideOverlay() {
+	
+// 	setTimeout((function(){
+// 		console.log(name);
+// 		console.log(pass);
+// 		if(name == "BREV" && pass =="BREV") {
+// 			document.getElementById("header").innerHTML += "<div class=\"logined\">Привет, "+name+"!</div>";
+// 			document.getElementById("overl").className="hidden";
+// 		}	
+// 		else {alert("Invalid user "+name+"! Enter again");}
 		
-	}), 1000);
-}
+// 	}), 1000);
+//}
 
 function showAsides() {
 	if(getComputedStyle(document.getElementById("left-aside")).display=="none") {
