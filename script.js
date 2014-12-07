d237981d74ac581efdc564c98319a4151bc4b4cf
Yauhen <brovchik.ne@gmail.com>
2014-12-07 22:27:45 +0300

    added comments into JS code

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-var map = L.map('map');
-
+var map = L.map('map'); //инициализация кариы
 var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
 map.addLayer(osm);
 map.setView([53.93, 27.589], 11);
 
-var markers = {};
-var items;
-var session;
-var servUpdCounter = 0;
+var markers = {}; //объект для хранения маркеров
+var items; //массив для хранения объектов
+var session; //сущность сессии
+var servUpdCounter = 0; //счетчик обращений на сервер
 
 
 
-
+// показать/скрыть панель объектов
 function showAsides() {
     if (getComputedStyle(document.getElementById("left-aside")).display == "none") {
         document.getElementById("left-aside").style.display = "block";
     }
     else document.getElementById("left-aside").style.display = "none";
-    //if (getComputedStyle(document.getElementById("right-aside")).display == "none") {
-    //    document.getElementById("right-aside").style.display = "block";
-    //}
-    //else document.getElementById("right-aside").style.display = "none";
 }
 
 //enter на поле password
@@ -44,10 +39,10 @@ function LogIn() {
             if (code == 0) {
             	document.getElementById("logined").innerHTML = "Привет, " + session.getCurrUser().getName() + "!<br><a href=\"#\" onclick=\"LogOut()\">Выйти</a>";
                 document.getElementById("overl").className = "hidden";
-                //получение информации об объекте, публикация их в левый столбец
-                session.loadLibrary('itemIcon');
-                session.addListener("serverUpdated", updDataFromServer);
-                updateFlags(session);
+                //получение информации об объекте, публикация их на панель объектов
+                session.loadLibrary('itemIcon'); //библиотека для работы с иконками
+                session.addListener("serverUpdated", updDataFromServer); // прослушка обновлений сервера
+                updateFlags(session); //загрузка объектов
             }
             else { alert("Error"); }
         });
@@ -55,18 +50,19 @@ function LogIn() {
     }
 }
 
+// загрузка объектов
 function updateFlags(session) {
     session.updateDataFlags([{ type: 'type', data: 'avl_unit', flags: 0x00000411, mode: 0 }], function () {
         var obj = document.getElementById("objects");
-        var objectsData = '';
-        var point = [];
-        items = session.getItems('avl_unit');
+        var objectsData = ''; // аккумулятор для вывода данных
+        var point = []; // массив для хранения точек
+        items = session.getItems('avl_unit'); //получение объектов
         for (var i = 0; i < items.length; i++) {
-        	items[i].addListener("changePosition", changesPosition);
+        	items[i].addListener("changePosition", changesPosition); // прослушка обновления позиций объекта
             var itemName = '<span class="objectName">' + items[i].getName() + '</span>';
             var itemIconUrl = '<div class="itemIcon"><img src="' + items[i].getIconUrl(32) + '"/></div>';
             var imgUrl = items[i].getIconUrl(32);
-            if (items[i].getPosition()) {
+            if (items[i].getPosition()) { //проверка на существование объекта
             	var myIcon = L.icon({
 				    iconUrl: imgUrl,
 				    iconRetinaUrl: imgUrl,
@@ -78,20 +74,22 @@ function updateFlags(session) {
 				    shadowSize: [,],
 				    shadowAnchor: [, ]
 				});
-                var itemPositionX = 'x: ' + items[i].getPosition().x.toFixed(3) + '&deg;';
-                var itemPositionY = 'y: ' + items[i].getPosition().y.toFixed(3) + '&deg;';
-                map.setView([items[i].getPosition().y, items[i].getPosition().x], 20);
-                var itemSpeed = 'Скорость: ' + items[i].getPosition().s + ' км/ч';
-                var itemTime = wialon.util.DateTime.formatTime(items[i].getPosition().t);
-                var id = items[i].getId();
-                var ago = session.getServerTime() - items[i].getPosition().t;
+                var itemPositionX = 'x: ' + items[i].getPosition().x.toFixed(3) + '&deg;'; // координата х
+                var itemPositionY = 'y: ' + items[i].getPosition().y.toFixed(3) + '&deg;'; //координата у
+                map.setView([items[i].getPosition().y, items[i].getPosition().x], 20); // настройка вида карты
+                var itemSpeed = 'Скорость: ' + items[i].getPosition().s + ' км/ч'; //скорость объекта
+                var itemTime = wialon.util.DateTime.formatTime(items[i].getPosition().t); //время последнего сообщения unix-time
+                var id = items[i].getId(); // id объекта
+                var ago = session.getServerTime() - items[i].getPosition().t; // вычисление интервала времени
                 var daysAgo = ago / 86400 | 0;
                 var hoursAgo = (ago - 86400 * daysAgo) / 3600 | 0;
                 var minutesAgo = (ago - 86400 * daysAgo - 3600 * hoursAgo) / 60 | 0;
                 var secondsAgo = ago - 86400 * daysAgo - 3600 * hoursAgo - 60 * minutesAgo;
                 ago = (daysAgo > 0 ? daysAgo + ' дней ' : '') + (hoursAgo > 0 ? hoursAgo + ' часов ' : '') + (minutesAgo > 0 ? minutesAgo + ' минут ' : '') + secondsAgo ;
-                var timeString = 'Время: ' + itemTime + '<br><span id = "ago' + id + '" class = "coord">' + ago + ' сек. назад</span>';
+                var timeString = 'Время: ' + itemTime + '<br><span id = "ago' + id + '" class = "coord">' + ago + ' сек. назад</span>'; //строка времени
+                //маркер
                 markers[id] = L.marker([items[i].getPosition().y, items[i].getPosition().x], { icon: myIcon }).addTo(map).bindPopup("<b>" + items[i].getName() + "</b><br>" + itemPositionX + ' ' + itemPositionY + "<br>" + itemSpeed + "<br>" + timeString);
+                //добавление координат в массив
                 point.push([items[i].getPosition().y, items[i].getPosition().x]);
             }
             else {
@@ -107,18 +105,19 @@ function updateFlags(session) {
                 var y = '';
                 var timeString = '';
             }
+            //запись строки вывода в аккумулятор
             objectsData += '<li class="item" unit id="' + id + '" onclick="findObject(' + id + ')">' + itemIconUrl + '<div class="itemInfo" id="' + id + 'itemInfo">' + itemName + '<br><span class="coord">' + itemPositionX + ' ' + itemPositionY + '<br>' + itemSpeed + '</span><br><span class="timeStr">' + timeString + '</span></div></li>';
         }
+        // изменение границ карты
         var bounds = L.latLngBounds(point);
         map.fitBounds(bounds);
-        obj.innerHTML += objectsData;
-        document.getElementById("left-aside").style.display = "block";
+        obj.innerHTML += objectsData; //вывод строки на страницу
+        document.getElementById("left-aside").style.display = "block"; //изначально, если нет объектов, hidden
     });
 }
 
-
+// перемещение вида карты к маркеру
 function findObject(id) {
-    
     var item = wialon.core.Session.getInstance().getItem(id);
     if (item.getPosition()) {
         var itemPositionX = item.getPosition().x;
@@ -127,12 +126,8 @@ function findObject(id) {
     }
     else return null;
 }
-// var obj = document.getElementById("objects");
-// obj.addEventListener('click', function(event) {
-// 	console.log(this);
-// });.
 
-//сдвиг маркера на текущую позицию и изменение в панели объектов
+//сдвиг маркера на текущую позицию и изменения в панели объектов
 function changesPosition(event) {
     var itemName = event.getTarget().getName();
 	var posX = event.getTarget().getPosition().x;
